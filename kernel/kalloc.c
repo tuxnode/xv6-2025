e33351654607df7e93eb1f13ac72f9910e379eb2
Tux Node <tuxnode@outlook.com>
2026-02-24 18:04:33 +0800

    Finish memory allocator

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -9,6 +9,8 @@
 #include "riscv.h"
 #include "defs.h"
 
+#define STEAL_PAGE_NUM 10
+
 void freerange(void *pa_start, void *pa_end);
 
 extern char end[]; // first address after kernel.
@@ -21,12 +23,22 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+  uint nfree; // 记录拥有的空闲物理页数量
+} kmem[NCPU];
 
+/*
+  这里采取的思路是先将所有空闲链表挂在初始化的那个cpu上(利于cpu0)
+  当其他cpu核心发现freelist为空的时候，就去init的那个cpu上"偷取"物理页
+  采用批量偷取的方法
+*/
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  for(int i = 0; i < NCPU; ++i){
+    initlock(&kmem[i].lock, "kmem");
+    kmem[i].freelist = 0;
+    kmem[i].nfree = 0;
+  }
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -34,9 +46,11 @@ void
 freerange(void *pa_start, void *pa_end)
 {
   char *p;
+
   p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE){
     kfree(p);
+  }
 }
 
 // Free the page of physical memory pointed at by pa,
@@ -47,6 +61,8 @@ void
 kfree(void *pa)
 {
   struct run *r;
+  push_off();
+  uint64 core_id = cpuid();
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
@@ -56,10 +72,14 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  // 释放物理页到当前cpu的链表中
+  acquire(&kmem[core_id].lock);
+  r->next = kmem[core_id].freelist;
+  kmem[core_id].freelist = r;
+  kmem[core_id].nfree += 1;
+  release(&kmem[core_id].lock);
+
+  pop_off();
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -69,12 +89,61 @@ void *
 kalloc(void)
 {
   struct run *r;
+  push_off();
+  uint64 core_id = cpuid();
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+  acquire(&kmem[core_id].lock);
+
+  r = kmem[core_id].freelist;
+
+  if(r){
+    kmem[core_id].freelist = r->next;
+    kmem[core_id].nfree -= 1;
+  }
+
+  release(&kmem[core_id].lock);
+  if(!r){
+    // 从其他核心获取PAGE
+    for(int i = 0; i < NCPU; ++i){
+      if(i == core_id) continue;
+
+      if(kmem[i].nfree <= 0) continue;
+
+      acquire(&kmem[i].lock);
+      if(kmem[i].nfree > 0){
+
+        struct run *head = kmem[i].freelist; // 找到返回的指针
+        struct run *p = head;
+        int count = 1;
+        int num = (STEAL_PAGE_NUM > kmem[i].nfree) ? kmem[i].nfree : STEAL_PAGE_NUM;
+
+        // 遍历找到需要更新的指针
+        while(p->next && count < num){
+          p = p->next;
+          count += 1;
+        }
+
+        kmem[i].freelist = p->next;
+        kmem[i].nfree -= count;
+        release(&kmem[i].lock);
+
+        p->next = 0;  // 截断取走的指针
+        r = head;
+        if(count > 1){
+          struct run *stolen = head->next;
+          acquire(&kmem[core_id].lock);
+          // 挂上偷取的物理页
+          p->next = kmem[core_id].freelist;
+          kmem[core_id].freelist = stolen;
+          kmem[core_id].nfree += count - 1;
+          release(&kmem[core_id].lock);
+        }
+        break;
+      }
+      release(&kmem[i].lock);
+    }
+  }
+  pop_off();
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
